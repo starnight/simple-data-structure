f53402684f3412ce1c528c8119ee7d055897556e
StarNight <zack_pan@alumni.ncu.edu.tw>
2013-09-22 11:09:36 +0800

    Add the license declaration for each code file.

--- demo.c
+++ demo.c
@@ -1,3 +1,9 @@
+/*------------------------------------------------------------------------------
+  Copyright (c) 2013 The Simple Data Structure Project. All rights reserved.
+  Use of this source code is governed by a BSD-style license that can be found
+  in the LICENSE file.
+------------------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include "datastructure.h"
 
